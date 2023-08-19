af82bc2cf8a3b12255f4c00dc98c1c946a8e3f91
raja <memonabdulrehman250@gmail.com>
2023-08-19 16:08:53 +0500

    navbar fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,8 +3,7 @@ import Navbar from '@/components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
-import { AppProvider, useAppContext } from '../../context/context'
-import SmallBar from '@/components/SmallBar'
+import { AppProvider } from '../../context/context'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
